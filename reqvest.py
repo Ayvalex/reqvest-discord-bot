9b624954d26049a4e893c01bab07656ed8825f7e
Alexander Ayvazov <ayvalex2010@gmail.com>
2025-05-17 18:13:34 -0700

    Add basic logging

--- reqvest.py
+++ reqvest.py
@@ -2,9 +2,16 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger('reqvest_bot')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,13 +21,13 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot connected as {bot.user}")
+    logger.info(f"Bot connected as {bot.user}")
 
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
-    
+    logger.info(f"Received message from {message.author}: {message.content}")
     await bot.process_commands(message)
 
 bot.run(token)
